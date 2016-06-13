ee667ea441a9ac82a5b6e057d53f2bd509a14414
Jackson Oates <jackson.oates@gmail.com>
2016-06-13 09:32:41 -0700

    added mongo db connection, first route for creating inventory

--- micro_app.rb
+++ micro_app.rb
@@ -1,14 +1,25 @@
 require 'sinatra'
 require 'json'
+require 'mongo'
+require 'json/ext'
 
 module Micro
   class MyApp < Sinatra::Base
 
+    ## config DB
+    configure do
+      db = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'test')  
+      set :mongo_db, db[:test]
+     end
+
     ## Endpoints
     
-    post '/inventory/:id/?' do
+    post '/inventory/?' do
       content_type :json
       # create record
+       db = settings.mongo_db
+       result = db.insert_one params
+       db.find(:_id => result.inserted_id).to_a.first.to_json
     end
 
     get '/inventory/:id/?' do
