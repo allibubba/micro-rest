4dafe3b7844aafbda0a11efdb29004d29ea4876e
Jackson Oates <jackson.oates@gmail.com>
2016-06-13 09:20:48 -0700

    routes set up

--- micro_app.rb
+++ micro_app.rb
@@ -1,6 +1,39 @@
 require 'sinatra'
+require 'json'
 
 module Micro
   class MyApp < Sinatra::Base
+
+    ## Endpoints
+    
+    post '/inventory/:id/?' do
+      content_type :json
+      # create record
+    end
+
+    get '/inventory/:id/?' do
+      content_type :json
+      # query inventory by id
+    end
+
+    put '/inventory/:id/?' do
+      content_type :json
+      # update record by id 
+    end
+
+    delete '/remove/:id' do
+      content_type :json
+      # delete record where id
+    end
+
+
+
+
+
+
+
+
+
+
   end
 end
